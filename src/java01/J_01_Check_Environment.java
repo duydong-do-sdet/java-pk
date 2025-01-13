2428d4a9663ac8c254453db053a710193d4f23c3
Do Duy Dong <duydong.do.sdet@gmail.com>
2025-01-13 15:29:30 +0700

    Check environment

---
+++ src/java01/J_01_Check_Environment.java
@@ -0,0 +1,13 @@
+package java01;
+
+public class J_01_Check_Environment {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("user.name"));
+
+		System.out.println(System.getProperty("os.name"));
+
+		System.out.println(System.getProperty("java.version"));
+	}
+
+}
