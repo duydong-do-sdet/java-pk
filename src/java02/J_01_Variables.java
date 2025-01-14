90a0a15d0ec244adf7a1537e9903e3d04c3eef23
Do Duy Dong <duydong.do.sdet@gmail.com>
2025-01-14 20:13:55 +0700

    Variables and Data Types

---
+++ src/java02/J_01_Variables.java
@@ -0,0 +1,28 @@
+package java02;
+
+public class J_01_Variables {
+
+	public static void main(String[] args) {
+		// #1
+		int a; // Declaration
+		a = 10; // Assignment
+		System.out.println("a = " + a);
+
+		// #2
+		int b = 20;
+		System.out.println("b = " + b);
+
+		// #3
+		int c, d;
+		c = 30;
+		d = 40;
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+
+		// #4
+		int e = 50, f = 60;
+		System.out.println("e = " + e);
+		System.out.println("f = " + f);
+	}
+
+}
---
+++ src/java02/J_02_Data_Types.java
@@ -0,0 +1,36 @@
+package java02;
+
+public class J_02_Data_Types {
+
+	public static void main(String[] args) {
+		// Primitive Types
+		byte bNumber = 127;
+		System.out.println(bNumber);
+
+		short sNumber = 32767;
+		System.out.println(sNumber);
+
+		int iNumber = 2147483647;
+		System.out.println(iNumber);
+
+		long lNumber = 9223372036854775807L;
+		System.out.println(lNumber);
+
+		float fNumber = 1.23456789F;
+		System.out.println(fNumber);
+
+		double dNumber = 1.2345678910111213;
+		System.out.println(dNumber);
+
+		boolean value = true;
+		System.out.println(value);
+
+		char letter = 'J';
+		System.out.println(letter);
+
+		// Reference Types
+		String text = "Java Programming";
+		System.out.println(text);
+	}
+
+}
